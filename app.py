161f5f65dd7690cba446538ac7d12e3a435e574b
Kar Sheng Lee <kar.sheng@contextlabs.com>
2022-06-06 16:49:31 +0200

    [FIX] heroku deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import time
 from flask import Flask
 
-app = Flask(__name__, static_folder='../web-app/build', static_url_path='/')
+app = Flask(__name__, static_folder='web-app/build', static_url_path='/')
 
 @app.route('/time')
 def get_current_time():
