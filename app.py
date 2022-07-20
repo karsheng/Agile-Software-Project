c373cac2d9d65d6e61f8ddc8fb0d28d93019e97f
Kar Sheng Lee <kar.sheng@contextlabs.com>
2022-07-20 18:05:47 +0200

    [FEAT] Auth backend

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from functools import wraps
 import json
 import pickle
 import time
 import pyrebase
+from firebase_admin import credentials, auth
+import firebase_admin
 
 import pandas as pd
 import plotly.express as px
@@ -13,31 +16,36 @@ app = Flask(__name__, static_folder='web-app/build', static_url_path='/')
 
 # Firebase config start
 # Firebase Api credentials for authorization. TODO set them as environment variables instead of passing directly.
-config = {
-    'apiKey': 'AIzaSyBNExpa-j77UVxVP5hlaPl3I4lDqLf84jc',
-    'authDomain': 'moneymood-662be.firebaseapp.com',
-    'projectId': 'moneymood-662be',
-    'storageBucket': 'moneymood-662be.appspot.com',
-    'messagingSenderId': '126870422755',
-    'appId': '1:126870422755:web:d655d169372ac38132f1d6',
-    'measurementId': 'G-6RP6EPN2G2',
-    'databaseURL': " "
-}
-
-firebase = pyrebase.initialize_app(config)  # Authenticate the api
-
-auth = firebase.auth()
-
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
 
 # Firebase config end
 
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            user = auth.verify_id_token(request.headers['authorization'])
+            print(user)
+            print('hello')
+            request.user = user
+        except Exception as e:
+            print(e)
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
 
 @app.route('/api/time')
+@check_token
 def get_current_time():
     return {'time': time.time()}
 
 
 @app.route('/api/btc_viz')
+@check_token
 def get_btc_viz():
     btc_prices = pd.read_csv("data/btc_prices.csv", parse_dates=["Timestamp"]).set_index("Timestamp")
     threshold = 0.10
@@ -55,47 +63,51 @@ def get_btc_viz():
 
 
 @app.route('/api/btc_sentiment')
+@check_token
 def get_btc_sentiment():
     fig = pickle.load(open('data/sentiment.pickle', 'rb'))
     return json.loads(fig.to_json())
 
 
-# App route to the login page, Upon successful authentication routes to homepage along with custom user session token.
-'''TODO Implement further verification, Probably with session tokens feature of firebase,only allow user with valid 
-tokens to view data. '''
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    unsuccessful = 'Please check your credentials'
-    if request.method == 'POST':
-        email = request.form['Email']
-        password = request.form['password']
-        try:
-            auth.sign_in_with_email_and_password(email, password)
-            return app.send_static_file('index.html')
-        except:
-            return render_template('login.html', us=unsuccessful)
-
-    return render_template('login.html')
-
-
-# App route to create a user account with email and password.
-@app.route('/signup', methods=['GET', 'POST'])
+#Api route to sign up a new user
+@app.route('/api/signup')
 def signup():
-    if request.method == 'POST':
-        email = request.form['Email']
-        password = request.form['password']
-        auth.create_user_with_email_and_password(email, password)
-        return app.send_static_file('index.html')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'}, 400
+    try:
+        user = auth.create_user(
+               email=email,
+               password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'}, 200
+    except Exception as e:
+        return {'message': 'Error creating user'}, 400
 
-    return render_template('signup.html')
+
+#Api route to get a new token for a valid user
+@app.route('/api/signin')
+def signin():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return {'token': jwt}, 200
+    except:
+        return {'message': 'There was an error logging in'},400
+
+@app.route('/api/userinfo')
+@check_token
+def userinfo():
+    return {'data': 'hello'}, 200
 
 
 # Default route has been updated to the login page
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
