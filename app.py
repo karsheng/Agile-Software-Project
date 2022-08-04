f2578c391f5b5c8fa661bb5a161825da9527dc39
Kar Sheng Lee <kar.sheng@contextlabs.com>
2022-08-04 17:45:12 +0200

    [FEAT] backend code for dashboard

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from functools import wraps
 import json
-import pickle
-import time
-import pyrebase
 from firebase_admin import credentials, auth, db
 import firebase_admin
 
@@ -12,21 +9,13 @@ from flask import *
 from dotenv import load_dotenv
 
 from src.utils import get_rise_timestamps, get_crash_timestamps
+from src.viz import subjectivity_polarity_plot, price_polarity_plot
+
 import os
 
 app = Flask(__name__, static_folder='web-app/build', static_url_path='/')
 
 load_dotenv()
-fb_config = {
-    'apiKey': os.environ['apiKey'],
-    'authDomain': os.environ['authDomain'],
-    'projectId': os.environ['projectId'],
-    'storageBucket': os.environ['storageBucket'],
-    'messagingSenderId': os.environ['messagingSenderId'],
-    'appId': os.environ['appId'],
-    'measurementId': os.environ['measurementId'],
-    'databaseURL': os.environ['databaseURL'],
-}
 
 # Firebase config start
 # Firebase Api credentials for authorization. TODO set them as environment variables instead of passing directly.
@@ -34,9 +23,6 @@ cred = credentials.Certificate('fbAdminConfig.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.environ['databaseURL']
 })
-
-pb = pyrebase.initialize_app(fb_config)
-
 # Firebase config end
 
 
@@ -55,12 +41,6 @@ def check_token(f):
     return wrap
 
 
-@app.route('/api/time')
-@check_token
-def get_current_time():
-    return {'time': time.time()}
-
-
 @app.route('/api/price_viz', methods=['GET'])
 @check_token
 def get_product_price_viz():
@@ -69,100 +49,48 @@ def get_product_price_viz():
     start = args.get('start')
     end = args.get('end')
 
-    ref = db.reference(
-        f'/prices/{product}').order_by_child('date').start_at(start).end_at(end)
-    df = pd.DataFrame(ref.get().values())
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.set_index('date')
+    ref1 = db.reference(
+        f'/{product}/prices').order_by_child('date').start_at(start).end_at(end)
+
+    ref2 = db.reference(
+        f'/{product}/polarity').order_by_child('date').start_at(start).end_at(end)
+
+    prices = pd.DataFrame(ref1.get().values())
+    prices['date'] = pd.to_datetime(prices['date'])
+    prices = prices.set_index('date').squeeze()
+
+    polarity = pd.DataFrame(ref2.get().values())
+    polarity['date'] = pd.to_datetime(polarity['date'])
+    polarity = polarity.set_index('date').squeeze()
 
     threshold = 0.12
-    ts = df.resample('7D').first()
+    ts = prices.resample('7D').first()
 
     rise_timestamps = get_rise_timestamps(ts, threshold)
     crash_timestamps = get_crash_timestamps(ts, threshold)
 
-    fig = px.line(df)
-    for t in rise_timestamps:
-        fig.add_vrect(
-            x0=t, x1=t + pd.Timedelta("7d"),
-            fillcolor="lightgreen", opacity=0.5,
-            layer="below", line_width=0,
-        )
-    for t in crash_timestamps:
-        fig.add_vrect(
-            x0=t, x1=t + pd.Timedelta("7d"),
-            fillcolor="salmon", opacity=0.5,
-            layer="below", line_width=0,
-        )
-    fig.update_yaxes({
-        "title": "Price (USD)"
-    })
+    fig = price_polarity_plot(
+        prices, polarity, rise_timestamps, crash_timestamps)
     return json.loads(fig.to_json())
 
 
-@app.route('/api/btc_viz')
+@app.route('/api/sentiment', methods=["GET"])
 @check_token
-def get_btc_viz():
-    btc_prices = pd.read_csv("data/btc_prices.csv",
-                             parse_dates=["Timestamp"]).set_index("Timestamp")
-    threshold = 0.10
-    ts = btc_prices.resample('7D').first()
+def get_sentiment_viz():
+    args = request.args
+    product = args.get('product')
+    start = args.get('start')
+    end = args.get('end')
 
-    rise_timestamps = get_rise_timestamps(ts, threshold)
-    fig = px.line(btc_prices)
-    for t in rise_timestamps:
-        fig.add_vrect(
-            x0=t, x1=t + pd.Timedelta("7d"),
-            fillcolor="lightgreen", opacity=0.5,
-            layer="below", line_width=1,
-        )
+    ref = db.reference(
+        f'/{product}/tweets').order_by_child('date').start_at(start).end_at(end)
+
+    df = pd.DataFrame(ref.get().values())
+    df['date'] = pd.to_datetime(df['date'])
+    fig = subjectivity_polarity_plot(df, 'Tweets sentiment')
     return json.loads(fig.to_json())
 
 
-@app.route('/api/btc_sentiment')
-@check_token
-def get_btc_sentiment():
-    fig = pickle.load(open('data/sentiment.pickle', 'rb'))
-    return json.loads(fig.to_json())
-
-
-# Api route to sign up a new user
-@app.route('/api/signup')
-def signup():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or password is None:
-        return {'message': 'Error missing email or password'}, 400
-    try:
-        user = auth.create_user(
-            email=email,
-            password=password
-        )
-        return {'message': f'Successfully created user {user.uid}'}, 200
-    except Exception as e:
-        return {'message': 'Error creating user'}, 400
-
-
-# Api route to get a new token for a valid user
-@app.route('/api/signin')
-def signin():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        user = pb.auth().sign_in_with_email_and_password(email, password)
-        jwt = user['idToken']
-        return {'token': jwt}, 200
-    except:
-        return {'message': 'There was an error logging in'}, 400
-
-
-@app.route('/api/userinfo')
-@check_token
-def userinfo():
-    return {'data': 'hello'}, 200
-
-
-# Default route has been updated to the login page
 @app.route('/')
 def index():
     return render_template('index.html')
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,44 @@
 import pandas as pd
 
+
 def get_crash_timestamps(
-    ts: pd.Series, 
-    threshold: float
-)-> pd.DatetimeIndex:
-    
-    # compute the percentage change of 
-    # prices for each row
-    diff = (ts.diff().shift(-1)/ts)
-    
-    # returns only the timestamps where the 
-    # changes is crosses the threshold
-    mask = diff  < -threshold
-    timestamps = mask[mask].dropna().index
-    
-    return timestamps
-    
-def get_rise_timestamps(
-    ts: pd.Series, 
+    ts: pd.Series,
     threshold: float
 ) -> pd.DatetimeIndex:
-    
-    # compute the percentage change of 
+
+    # compute the percentage change of
     # prices for each row
     diff = (ts.diff().shift(-1)/ts)
 
-    # returns only the timestamps where the 
+    # returns only the timestamps where the
     # changes is crosses the threshold
-    mask =  diff > threshold
+    mask = diff < -threshold
     timestamps = mask[mask].dropna().index
-    
+
     return timestamps
+
+
+def get_rise_timestamps(
+    ts: pd.Series,
+    threshold: float
+) -> pd.DatetimeIndex:
+
+    # compute the percentage change of
+    # prices for each row
+    diff = (ts.diff().shift(-1)/ts)
+
+    # returns only the timestamps where the
+    # changes is crosses the threshold
+    mask = diff > threshold
+    timestamps = mask[mask].dropna().index
+
+    return timestamps
+
+
+def get_sentiment(score: float) -> str:
+    if score < -0.33:
+        return 'negative'
+    elif score >= -0.33 and score <= 0.33:
+        return "neutral"
+    else:
+        return "positive"
