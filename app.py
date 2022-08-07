be8797559c63e71c3976e18ff9ea248e50dd8662
Kar Sheng Lee <kar.sheng@contextlabs.com>
2022-08-07 21:43:04 +0200

    [CHORES] update env

--- app.py
+++ app.py
@@ -18,7 +18,23 @@ load_dotenv()
 
 # Firebase config start
 # Firebase Api credentials for authorization. TODO set them as environment variables instead of passing directly.
-cred = credentials.Certificate('fbAdminConfig.json')
+
+cred_dict = {
+    'type': os.environ['type'],
+    'project_id': os.environ['project_id'],
+    'private_key_id': os.environ['private_key_id'],
+    'private_key': os.environ.get('private_key').encode('latin1').decode('unicode_escape'),
+    'client_email': os.environ['client_email'],
+    'client_id': os.environ['client_id'],
+    'auth_uri': os.environ['auth_uri'],
+    'token_uri': os.environ['token_uri'],
+    'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
+    'client_x509_cert_url': os.environ['client_x509_cert_url']
+}
+
+
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.environ['databaseURL']
 })
